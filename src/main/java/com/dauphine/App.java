f14c9c3ce312016755fdc9b779656e04fe74a522
belgacea <adam.belgacem@gmail.com>
2017-04-16 03:23:59 +0200

    Tests again

--- src/main/java/com/dauphine/App.java
+++ src/main/java/com/dauphine/App.java
@@ -42,7 +42,7 @@ public class App {
 //            dropDatabase();
 //        }
         int s = Integer.parseInt(args[0]);
-        logger.info("Trying the case n°" + s);
+        logger.info("Trying case n°" + s);
         try {
             switch (s){
                 case 0:
@@ -68,20 +68,49 @@ public class App {
     }
 
     private static void case1() {
+        //Init
         int[] id1 = init1();
-        tryApprenti(id1[0]);
-        tryEntrepriseLazy(id1[1]);
-        tryApprentissageLazy(id1);
+        int[] id2 = init2();
+        int[] id3 = test1();
+
+        Apprenti apprenti1 = tryApprenti(id1[0]);
+        Entreprise entreprise1 = tryEntrepriseLazy(id1[1]);
+        Apprentissage apprentissage1 = tryApprentissageLazy(id1);
+
+        Apprenti apprenti2 = tryApprenti(id2[0]);
+        Entreprise entreprise2 = tryEntrepriseLazy(id2[1]);
+        Apprentissage apprentissage2 = tryApprentissageLazy(id2);
+
+        Apprenti apprenti3 = tryApprenti(id3[0]);
+        Apprenti apprenti4 = tryApprenti(id3[1]);
+
     }
 
     private static void case2() {
-        int[] id2 = init2();
-        tryApprenti(id2[0]);
-        tryEntrepriseLazy(id2[1]);
-        tryApprentissageLazy(id2);
+        //Tests
+        Apprenti apprenti1 = tryApprenti(1);
+        Entreprise entreprise1 = tryEntrepriseLazy(1);
+        Apprentissage apprentissage1 = tryApprentissageLazy(new int[] {1,1});
+        logger.debug("Apprenti test1 = " + apprenti1.equals(apprentissage1.getApprenti()));
+        logger.debug("Entreprise test1 = " + entreprise1.equals(apprentissage1.getEntreprise()));
+        logger.debug("Apprentissage test1 = " + apprentissage1.equals(apprenti1.getApprentissage()));
+
+        Apprenti apprenti2 = tryApprenti(2);
+        Entreprise entreprise2 = tryEntrepriseLazy(2);
+        Apprentissage apprentissage2 = tryApprentissageLazy(new int[] {2,2});
+        logger.debug("Apprenti test2 = " + apprenti2.equals(apprentissage2.getApprenti()));
+        logger.debug("Entreprise test2 = " + entreprise2.equals(apprentissage2.getEntreprise()));
+        logger.debug("Apprentissage test2 = " + apprentissage2.equals(apprenti2.getApprentissage()));
+
+        Apprenti apprenti3 = tryApprenti(3);
+        logger.debug("Entreprise test3 = " + entreprise1.equals(apprenti3.getApprentissage().getEntreprise()));
+        Apprenti apprenti4 = tryApprenti(4);
+        logger.debug("Entreprise test4 = " + entreprise1.equals(apprenti4.getApprentissage().getEntreprise()));
     }
 
     private static void case3(){
+        //Summary
+        tryCount();
         tryAllApprenti();
         tryAllEntreprise();
         tryAllApprentissage();
@@ -97,8 +126,8 @@ public class App {
         apprentiDAO.create(belgacem);
         apprentissageDAO.create(appr1);
 
-        logger.debug("IDA = "+ belgacem.getId());
-        logger.debug("IDE = "+ bdf.getId());
+//        logger.debug("IDA = "+ belgacem.getId());
+//        logger.debug("IDE = "+ bdf.getId());
 
         return new int[] {belgacem.getId(), bdf.getId()};
     }
@@ -113,14 +142,41 @@ public class App {
         apprentiDAO.create(madkour);
         apprentissageDAO.create(appr0);
 
-        logger.debug("IDA = "+ madkour.getId());
-        logger.debug("IDE = "+ ca.getId());
+//        logger.debug("IDA = "+ madkour.getId());
+//        logger.debug("IDE = "+ ca.getId());
 
         return new int[] {madkour.getId(), ca.getId()};
     }
 
-    private static void tryAllApprenti(){
+    private static int[] test1() {
+        Entreprise bdf = entrepriseDAO.findLazy(1);
+        Apprenti jean = new Apprenti("D'Armery", "Jean");
+        MaitreApp emma = new MaitreApp("Carena", "Emma");
+        Apprentissage appr1 = new Apprentissage(bdf, jean, emma);
+
+        apprentiDAO.create(jean);
+        apprentissageDAO.create(appr1);
+
+        Apprenti john = new Apprenti("Doeuf", "John");
+        MaitreApp terry = new MaitreApp("Golo", "Terry");
+        Apprentissage appr2 = new Apprentissage(bdf, john, terry);
+
+        apprentiDAO.create(john);
+        apprentissageDAO.create(appr2);
+
+//        logger.debug("IDA = "+ jean.getId());
+//        logger.debug("IDE = "+ bdf.getId());
+
+        return new int[] {jean.getId(), john.getId()};
+    }
+
+    private static void tryCount() {
         logger.debug("Apprenti =" + apprentiDAO.countAll());
+        logger.debug("Entreprise =" + entrepriseDAO.countAll());
+        logger.debug("Apprentissage =" + apprentissageDAO.countAll());
+    }
+
+    private static void tryAllApprenti(){
         List<Apprenti> apprentis = apprentiDAO.findAll();
         for(Iterator<Apprenti> i = apprentis.iterator(); i.hasNext();) {
             logger.debug(i.next().toString());
@@ -128,7 +184,6 @@ public class App {
     }
 
     private static void tryAllEntreprise(){
-        logger.debug("Entreprise =" + entrepriseDAO.countAll());
         List<Entreprise> entreprises = entrepriseDAO.findAll();
         for(Iterator<Entreprise> i = entreprises.iterator(); i.hasNext();) {
             logger.debug(i.next().toString());
@@ -136,28 +191,30 @@ public class App {
     }
 
     private static void tryAllApprentissage(){
-        logger.debug("Apprentissage =" + apprentissageDAO.countAll());
         List<Apprentissage> apprentissages = apprentissageDAO.findAll();
         for(Iterator<Apprentissage> i = apprentissages.iterator(); i.hasNext();) {
             logger.debug(i.next().toString());
         }
     }
 
-    private static void tryApprenti(int id) {
+    private static Apprenti tryApprenti(int id) {
         Apprenti test = apprentiDAO.find(id);
-        logger.info(test.toString());
+        logger.debug(test.toString());
+        return test;
     }
 
-    private static void tryEntrepriseLazy(int id) {
+    private static Entreprise tryEntrepriseLazy(int id) {
         Entreprise test = entrepriseDAO.findLazy(id);
-        logger.info(test.toString());
-        logger.info("Java = " + test.countApprentissages());
-        logger.info("API Criteria = " + apprentissageDAO.countById(test.getId()));
+        logger.debug(test.toString());
+        logger.info("[Count Apprentissage] Java = " + test.countApprentissages());
+        logger.info("[Count Apprentissage] API Criteria = " + apprentissageDAO.countById(test.getId()));
+        return test;
     }
 
-    private static void tryApprentissageLazy(int[] ids) {
+    private static Apprentissage tryApprentissageLazy(int[] ids) {
         Apprentissage test = apprentissageDAO.findLazy(ids[1], ids[0]);
-        logger.info(test.toString());
+        logger.debug(test.toString());
+        return test;
     }
 
 //    private static void tryEntrepriseEager(int id) {
